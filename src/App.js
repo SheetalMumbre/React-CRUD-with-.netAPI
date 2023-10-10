7fc927b731d6110b43571d13f813fca4ce6da1db
Sheetal <sheetal.bawdane@tangible.tech>
2023-10-10 12:29:18 +0530

    Add all  CRUD operations

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { store } from './actions/store';
 import { Provider } from 'react-redux';
 import DCandidates from './components/dCandidates';
 import { Container } from '@material-ui/core';
+import { ToastProvider } from 'react-toast-notifications';
 
 function App() {
   return (
     <Provider store={store}>
-      <Container maxWidth="lg">
-          <DCandidates/>
-      </Container>
+      <ToastProvider autoDismiss={true}>
+        <Container maxWidth="lg">
+            <DCandidates/>
+        </Container>
+      </ToastProvider>
     </Provider>
   );
 }
--- src/components/useForm.js
+++ src/components/useForm.js
@@ -1,15 +1,25 @@
 import React, {useState, useEffect} from "react";
 
-const useForm = (initialFieldValues) => {
+const useForm = (initialFieldValues,validate,setCurrentId) => {
     const[values, setValues] = useState(initialFieldValues);
     const[errors, setErrors] = useState({})
 
     const handleInputChange = e => {
         const {name, value} = e.target
+        const fieldValue = {[name]: value}
         setValues({
             ...values,
-            [name] : value
+            ...fieldValue
         })
+        validate(fieldValue)
+    }
+
+    const resetForm = () => {
+        setValues({
+            ...initialFieldValues
+        })
+        setErrors({})
+        setCurrentId(0)
     }
 
     return{
@@ -17,7 +27,8 @@ const useForm = (initialFieldValues) => {
          setValues,
          errors,
          setErrors,
-         handleInputChange
+         handleInputChange,
+         resetForm
     };
 }
 
